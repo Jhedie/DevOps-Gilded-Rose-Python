21d0164bbfc090168a4470904045ff9b724eff2c
Jhedie <jeddiahawuku12@gmail.com>
2025-02-04 22:43:24 +0000

    Refactor quality update logic to use static methods for increasing and decreasing quality

--- gilded_rose/gilded_rose.py
+++ gilded_rose/gilded_rose.py
@@ -3,32 +3,33 @@ class GildedRose(object):
     def __init__(self, items):
         self.items = items
 
+    @staticmethod
+    def increase_quality(item):
+        item.quality += 1
+
+    @staticmethod
+    def decrease_quality(item):
+        item.quality -= 1
+
     def update_quality(self):
         for item in self.items:
             if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
+                if item.quality > 0 and item.name != "Sulfuras, Hand of Ragnaros":
+                    self.decrease_quality(item)
+            elif item.quality < 50:
+                self.increase_quality(item)
+                if item.name == "Backstage passes to a TAFKAL80ETC concert" and item.quality < 50:
+                    if item.sell_in < 11:
+                        self.increase_quality(item)
+                    if item.sell_in < 6:
+                        self.increase_quality(item)
             if item.name != "Sulfuras, Hand of Ragnaros":
                 item.sell_in = item.sell_in - 1
             if item.sell_in < 0:
                 if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
+                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
                         item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+                    if item.name != "Sulfuras, Hand of Ragnaros" and item.quality > 0:
+                        self.decrease_quality(item)
+                elif item.quality < 50:
+                    self.increase_quality(item)
